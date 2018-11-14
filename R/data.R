dc7e4985091727394afc66a04c9c61eeae8a6343
Fabian Müller <fmueller@mpi-inf.mpg.de>
2018-11-14 11:24:30 -0800

    initial commit

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' \code{Gviz} annotation data for geneome assembly hg38
+#'
+#' @format A \code{GvizManager} object with annotation data for genome assembly
+#' \describe{
+#'   \item{gene annotation}{based on GENCODE v27}
+#' }
+"gvm_hg38"
+
+#' \code{Gviz} annotation data for geneome assembly hg19
+#'
+#' @format A \code{GvizManager} object with annotation data for genome assembly
+#' \describe{
+#'   \item{gene annotation}{based on GENCODE v19}
+#' }
+"gvm_hg19"
+
+#' \code{Gviz} annotation data for geneome assembly mm10
+#'
+#' @format A \code{GvizManager} object with annotation data for genome assembly
+#' \describe{
+#'   \item{gene annotation}{based on GENCODE M16}
+#' }
+"gvm_mm10"
